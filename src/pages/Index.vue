2212f62be1ffb2f3bb4d0b9d32ae9c12e22ccc62
Luka Bacic <luka.bacic278@gmail.com>
2020-07-30 11:09:52 +1000

    Remove stray p tag

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,17 @@
 <template>
   <Layout>
     <p>Open dev tools and go to Network Tab. Reload page.</p>
-    <p>You can see the 2 images loaded immediately (<strong>portrait-5378357_640.jpg</strong> and <strong>girl-2696947_640.jpg</strong>).</p> Lazy loading does not work with them.</p>
-    <p>The static image below them is lazy loaded only when it comes into the viewport</p>
+    <p>
+      You can see the 2 images loaded immediately (<strong
+        >portrait-5378357_640.jpg</strong
+      >
+      and <strong>girl-2696947_640.jpg</strong>).
+    </p>
+    Lazy loading does not work with them.
+    <p>
+      The static image below them is lazy loaded only when it comes into the
+      viewport
+    </p>
     <div class="spacer"></div>
     <div class="spacer"></div>
     <div class="spacer"></div>
